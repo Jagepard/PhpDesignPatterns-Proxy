937ac4624ddb4b5c175663fbdf9a6f7d6647d5f3
Jagepard <dankorot@gmail.com>
2019-03-01 16:07:17 +0300

    Update

--- src/SubjectInterface.php
+++ src/SubjectInterface.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * @author    : Korotkov Danila <dankorot@gmail.com>
  * @license   https://mit-license.org/ MIT
@@ -15,7 +13,6 @@ namespace Structural\Proxy;
  */
 interface SubjectInterface
 {
-
     /**
      * @param string $value
      * @param string $key
